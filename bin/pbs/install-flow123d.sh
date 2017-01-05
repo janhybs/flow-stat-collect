1aac154242a7d6d683fd06bc0821b7ff40326646
Jan_Hybs <jan.hybs@tul.cz>
2017-01-05 16:04:18 +0100

    Edit time limit

--- bin/pbs/install-flow123d.sh
+++ bin/pbs/install-flow123d.sh
@@ -6,7 +6,7 @@
 #PBS -l walltime=01:30:00
 #PBS -j oe
 # ::  qsub -l walltime=1h -l nodes=1:ppn=3,mem=4gb -I
-# ::  qsub -l walltime=1:00:00 -l select=1:ncpus=2:mem=4gb -I
+# ::  qsub -l walltime=1:59:00 -l select=1:ncpus=2:mem=4gb -I
 
 # path where flow123d repository is
 FLOW_LOC=$1
