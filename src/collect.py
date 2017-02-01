f9c7beeaca06129da2e3a55936b58cfdc469498f
Jan_Hybs <jan.hybs@tul.cz>
2017-02-01 10:57:56 +0100

    Add option to only collect results

--- src/collect.py
+++ src/collect.py
@@ -12,6 +12,7 @@ __dir__ = dirname(__file__)
 __root__ = dirname(__dir__)
 sys.path.append(abspath(join(__root__, 'libs')))
 
+
 def main():
     from tul.flow123d.utils.strings import random_hex
     from tul.flow123d.common.config import cfg
@@ -33,17 +34,20 @@ def main():
     """)
     parser.add_argument('-a', '--all', dest='opts', action='store_const',
                         default=default_options, const=default_options_all, help="""
-        If set will run all tags for all tests (by default only tests with tag 'benchmark' will used.
+        If set will run all tags for all tests (by default only tests with tag 'benchmark' will used).
     """)
     parser.add_argument('-r', '--repeat', metavar='N', default=1, type=int, help="""
         Number indicating how many times will be runtest script called. Default behaviour is one time.
     """)
+    parser.add_argument('-s', '--only-collect', action='store_true', help="""
+        If set will not run tests only collect results.
+    """)
 
     # parse and fix list of tests
-    arg_options = parser.parse_args()
-    arg_options.test = ['bench_data/benchmarks'] if not arg_options.test else arg_options.test
-    arg_options.flow = arg_options.flow or cfg.get_flow123d_root()
-    flow_root = arg_options.flow
+    args = parser.parse_args()
+    args.test = ['bench_data/benchmarks'] if not args.test else args.test
+    args.flow = args.flow or cfg.get_flow123d_root()
+    flow_root = args.flow
 
     # runtest wrapper
     runtest = abspath(join(flow_root, 'tests/runtest'))
@@ -56,30 +60,33 @@ def main():
     """.strip()
 
     total = 0
-    for i in range(arg_options.repeat):
-        if arg_options.repeat > 1:
-            print("Repeat %02d/%02d" % (i+1, arg_options.repeat))
+    for i in range(args.repeat):
+        if args.repeat > 1:
+            print("Repeat %02d/%02d" % (i+1, args.repeat))
 
-        # prepare random variables
-        rnd = random_hex(8)
-        eq_lambda = [lambda x: str(x).find(rnd) != -1]
+        if args.only_collect:
+            eq_lambda = []
+        else:
+            # prepare random variables
+            rnd = random_hex(8)
+            eq_lambda = [lambda x: str(x).find(rnd) != -1]
 
-        # build command
-        command = [runtest] + arg_options.test
-        command = command + arg_options.opts.split()
-        command = command + ['--random-output-dir', rnd]
-        command_str = ' '.join(command)
+            # build command
+            command = [runtest] + args.test
+            command = command + args.opts.split()
+            command = command + ['--random-output-dir', rnd]
+            command_str = ' '.join(command)
 
-        print(info_template.format(**locals()))
-        print('=' * 80)
+            print(info_template.format(**locals()))
+            print('=' * 80)
 
-        # run process
-        process = subprocess.Popen(command, cwd=flow_root)
-        process.wait()
+            # run process
+            process = subprocess.Popen(command, cwd=flow_root)
+            process.wait()
 
         def load_data():
             data = list()
-            for t in arg_options.test:
+            for t in args.test:
                 instance = profiler.Flow123dProfiler()
                 path = join(flow_root, t)
                 items = loader.load_data(path, instance, eq_lambda)
@@ -95,16 +102,16 @@ def main():
                 return 0
 
         # check result
-        print('=' * 80)
-        if process.returncode == 0:
-            # if tests passed, we will save result to database
-            print('[ OK ]    | Process ended successfully')
-            total += load_data()
-        else:
-            # print error on error
-            print('[ WARN ] | Error while executing command, return code %d' % process.returncode)
-            total += load_data()
-
+        if not args.only_collect:
+            print('=' * 80)
+            if process.returncode == 0:
+                # if tests passed, we will save result to database
+                print('[ OK ]    | Process ended successfully')
+            else:
+                # print error on error
+                print('[ WARN ] | Error while executing command, return code %d' % process.returncode)
+        print('[ OK ] | Collecting data')
+        total += load_data()
     print('=' * 80)
     print("Inserted total of %d documents" % total)
 
